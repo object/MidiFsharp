8adc77369a529c18ae976e809c596d6de07d9090
anton164 <anton_abilov@hotmail.com>
2017-04-01 11:59:19 +0200

    Simplified Feature type, takes an arbitrary function

--- MidiFsharp/Feature.fsx
+++ MidiFsharp/Feature.fsx
@@ -9,13 +9,10 @@ module Utils =
     open NAudio.Midi
 
     type FeatureValue = float
-
-    type IFeature = 
-        abstract member Calculate: MidiFile -> float 
-        abstract member Similarity: FeatureValue * FeatureValue -> float
+    type Feature(calculateFun) =
+            member this.Calculate(f) = calculateFun(f)
 
     type FeatureVector = FeatureValue * FeatureValue
-
 
     let magnitude (f: FeatureVector) =
         let fv1, fv2 = f
@@ -25,16 +22,13 @@ module Utils =
         let f1V1, f1V2 = f1
         let f2V1, f2V2 = f2
         float(f1V1 * f1V2 + f2V1 * f2V2)
-        
+
     let cosineSimilarity (f1: FeatureVector, f2: FeatureVector) =
         float (dotProduct(f1, f2) / (magnitude(f1) * magnitude(f2)))
 
-    type TestFeature() =
-        interface IFeature with
-            member this.Similarity(f1, f2) =
-                f1 * f2
-            member this.Calculate(f) =
-                0.2
-    
+    let robbie = MidiFile("./MidiFiles/feel.mid")
+    let elton = MidiFile("./MidiFiles/DontGoBreakingMyHeart.mid")
 
+    let feature1 = Feature(fun a -> 2.3)
+    let testVector1 = (feature1.Calculate(robbie), feature1.Calculate(robbie))
 
