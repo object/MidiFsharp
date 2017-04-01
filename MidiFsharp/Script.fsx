ebcd2a01ec72a50af317472b47f3cac15cb3a3de
Vagif Abilov <vagif.abilov@gmail.com>
2017-04-01 12:46:07 +0200

    Better charts.

--- MidiFsharp/Script.fsx
+++ MidiFsharp/Script.fsx
@@ -25,6 +25,47 @@ let notesAvgLength = file.Events|> Seq.skip 1 |> Seq.mapi (fun i ec -> sprintf "
 
 file |> getTrackEvents 3 |> getNotesRange
 
+let trackNames = [
+    "Piano 1"
+    "Bass"
+    "Strings"
+    "El.Guitar"
+    "Piano 2"
+    "Cymbal"
+    "Drums"
+]
+
+let features = [|
+    "Range"
+    "Min P"
+    "Max P"
+    "Avg P"
+//    "Avg L"
+|]
+
+let series = [ "bars";"bars";"bars";"bars";"bars";"bars";"bars" ]
+let inputs = 
+    file.Events
+    |> Seq.skip 1
+    |> Seq.map (fun ec -> 
+        [
+            (features.[0], getNotesRangeWidth ec |> float)
+            (features.[1], getNotesMinPitch ec |> float)
+            (features.[2], getNotesMaxPitch ec |> float)
+            (features.[3], getNotesAvgPitch ec)
+//            (features.[4], getNotesAvgLength ec)
+        ])
+
+
+inputs
+|> Chart.Combo
+|> Chart.WithOptions 
+     (Options(title = "Notes Stats", series = 
+        [| for typ in series -> Series(typ) |]))
+|> Chart.WithLabels trackNames
+|> Chart.WithLegend true
+|> Chart.WithSize (600, 250)
+
 Chart.Bar(notesRange)
 Chart.Bar(notesAvgPitch)
 Chart.Bar(notesAvgLength)
